cb3ffffa6e821baf3f6eacbe9b810ba57ad0ed6e
Evgeny <evgeny.zhoga@yandex.ru>
2013-07-10 09:19:09 +0200

    updated web.js

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var data = fs.readFileSync("index.html");
-  response.send(data);
+  response.send(data.toString("utf-8"));
 });
 
 var port = process.env.PORT || 5000;
