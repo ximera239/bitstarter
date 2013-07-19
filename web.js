11ad0e15b3d12701d8db4dd897bb67d0adc10789
Evgeny Zhoga <ezhoga@forteamwork.ru>
2013-07-19 15:47:54 +0200

    updated index.html/web.js for HW4

--- web.js
+++ web.js
@@ -8,7 +8,7 @@ app.get('/', function(request, response) {
   response.send(data.toString("utf-8"));
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 18080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
